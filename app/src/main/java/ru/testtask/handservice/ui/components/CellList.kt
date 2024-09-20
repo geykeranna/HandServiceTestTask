c4ab3715361065aacf2607ff2696e989d9e9e362
geykerann <geykerannam@gmail.com>
2024-09-20 15:43:22 +0300

    feat: Добавление анимаций небольших и скролла к концу списка при добавлении

--- app/src/main/java/ru/testtask/handservice/ui/components/CellList.kt
+++ app/src/main/java/ru/testtask/handservice/ui/components/CellList.kt
@@ -1,11 +1,15 @@
 package ru.testtask.handservice.ui.components
 
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -13,11 +17,19 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import ru.testtask.handservice.domain.models.Cell
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun CellList(
     cells: List<Cell>,
     modifier: Modifier = Modifier,
 ) {
+    val listState = rememberLazyListState()
+    LaunchedEffect(cells.size) {
+        if (cells.isNotEmpty()) {
+            listState.animateScrollToItem(cells.size - 1)
+        }
+    }
+
     LazyColumn(
         modifier = modifier
             .clip(RoundedCornerShape(4.dp))
@@ -25,10 +37,13 @@ fun CellList(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(4.dp),
         reverseLayout = true,
+        state = listState,
     ) {
         items(cells){ cell ->
             CellItem(
-                modifier = Modifier,
+                modifier = Modifier.animateItemPlacement(
+                    animationSpec = tween(durationMillis = 600)
+                ),
                 cell = cell,
             )
         }
--- app/src/main/java/ru/testtask/handservice/ui/screens/main/MainScreen.kt
+++ app/src/main/java/ru/testtask/handservice/ui/screens/main/MainScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
@@ -12,6 +13,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
